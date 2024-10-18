aecb10b8c6a5fd457409a316b91e54c6647d70a0
Lev Plyusnin <me@llistochek.ru>
2024-10-18 12:10:12 +0700

    Change response messages

--- src/main/java/com/example/pdfreplacerbot/TelegramBot.java
+++ src/main/java/com/example/pdfreplacerbot/TelegramBot.java
@@ -31,7 +31,7 @@ public class TelegramBot implements SpringLongPollingBot, LongPollingUpdateConsu
     private final BotConfig botConfig;
     private final Service service;
     private final TelegramClient client;
-    private final Map<Long, String> users = new HashMap<>();
+    private final Map<Long, Document> users = new HashMap<>();
 
     private final Executor executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
@@ -44,50 +44,59 @@ public class TelegramBot implements SpringLongPollingBot, LongPollingUpdateConsu
     public void consume(Update update) {
         if (update.hasMessage()) {
             Message message = update.getMessage();
+            Long chatId = message.getChatId();
+            if (message.hasDocument()) {
+                Document document = message.getDocument();
+                if (!document.getFileName().endsWith(".pdf")) {
+                    String response = """
+                            Неверный формат файла. Пришлите pdf документ.
+                            """;
+                    sendTextResponse(response, chatId);
+                    return;
+                }
+                users.put(message.getChatId(), document);
+
+                String response = """
+                        Напишите текст, который необходимо вставить в документ.
+                        """;
+                sendTextResponse(response, chatId);
+            }
             if (message.hasText()) {
                 if (message.getText().equals("/start")) {
                     String response = """
-                            Пришлите строку для замены в документе
+                            Загрузите исходный файл Честного Знака, в формате PDF.
                             """;
 
                     sendTextResponse(response, message.getChatId());
                 } else {
-                    users.put(message.getChatId(), message.getText());
-
-                    String response = """
-                            Пришлите pdf документ для обработки
-                            """;
-
-                    sendTextResponse(response, message.getChatId());
-                }
-            }
-            if (message.hasDocument()) {
-                if (users.get(message.getChatId()) != null) {
-                    Document document = message.getDocument();
-                    GetFile getFile = new GetFile(document.getFileId());
+                    String replacementText = message.getText();
+                    Document document = users.get(chatId);
+                    if (document == null) {
+                        String response = """
+                                Сначала загрузите файл, затем пришлите текст.
+                                """;
+                        sendTextResponse(response, message.getChatId());
+                        return;
+                    }
                     try {
+                        GetFile getFile = new GetFile(document.getFileId());
                         File file = client.execute(getFile); //tg file obj
                         java.io.File dFile = client.downloadFile(file);
 
-                        if (!document.getFileName().endsWith(".pdf")) {
-                            String response = """
-                            Неверный формат файла. Пришлите pdf документ
-                            """;
-                            sendTextResponse(response, message.getChatId());
-                            return;
-                        }
-
-                        java.io.File result = service.processPdf(dFile, users.get(message.getChatId()));
-                        sendFileResponse(result, message.getChatId(), document.getFileName());
-                        users.remove(message.getChatId());
+                        java.io.File result = service.processPdf(dFile, replacementText);
+                        sendFileResponse(result, chatId, document.getFileName());
+                        users.remove(chatId);
                         String response = """
-                            Обработка прошла успешно.
-                            Пришлите следующую строку для замены в документе
+                            Обработка прошла успешно. (пока что бот генерирует файл только в формате 58*40, если бот будет пользоваться популярностью, добавим еще размеры)
+                            
+                            Важно! Проверяйте несколько QR-кодов из файла с помощью приложения Честный Знак.
+                            Честный знак может внести изменения в алгоритмы работы кода, вам нужно убедиться, что файл, сгенерирован корректно. Если вы проверите несколько QR-кодов и они работают, то все остальные коды в этом файле будут работать правильно.
                             """;
-                        sendTextResponse(response, message.getChatId());
+                        sendTextResponse(response, chatId);
 
                         dFile.delete();
                         result.delete();
+                        users.remove(chatId);
                     } catch (Exception e) {
                         String response = """
                             Что-то пошло не так.
@@ -96,18 +105,10 @@ public class TelegramBot implements SpringLongPollingBot, LongPollingUpdateConsu
                         sendTextResponse(response + e.getMessage(), message.getChatId());
                         throw new RuntimeException(e);
                     }
-                } else {
-                    String response = """
-                            Пришлите строку для замены в документе и повторите попытку
-                            """;
-                    sendTextResponse(response, message.getChatId());
-
                 }
-
             }
         }
     }
-
 
     @Override
     public String getBotToken() {
