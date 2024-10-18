1f4c2ba17fb47da9a44abd4dd52c1006af8ba626
Lev Plyusnin <me@llistochek.ru>
2024-10-18 11:31:51 +0700

    Remove files dir

--- src/main/java/com/example/pdfreplacerbot/PdfReplacerBotApplication.java
+++ src/main/java/com/example/pdfreplacerbot/PdfReplacerBotApplication.java
@@ -4,13 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.io.File;
-
 @SpringBootApplication
 public class PdfReplacerBotApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(PdfReplacerBotApplication.class, args);
-        (new File("files")).mkdir();
     }
 }
